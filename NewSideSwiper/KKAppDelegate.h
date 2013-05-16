a1673d80f1289b56781ccd7965fc1671d5c0451a
kal <kalyan.krishnamurthi@biascorp.com>
2013-05-16 09:46:59 -0500

    Initial Commit

---
+++ NewSideSwiper/KKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KKAppDelegate.h
+//  NewSideSwiper
+//
+//  Created by kalyankrishnamurthi on 5/16/13.
+//  Copyright (c) 2013 clrvynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
